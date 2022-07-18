a5b8d27a92fb8f33e8e32319b401e1ee19af3934
Mark Wilkinson <mark@highrise.digital>
2022-07-18 13:29:08 +0100

    Use HTML markup
    Also:
    
    * Check for post thumbnail and use if present as the background image
    * Coreect Google font URL output
    * Check taxonomy exists before attempting to replace with term values

--- endpoints/generate-html.php
+++ endpoints/generate-html.php
@@ -27,6 +27,14 @@ $args = array(
 	'logo'               => wp_get_attachment_image_url( get_option( 'ssi_wpjm_logo_id' ), 'full' ),
 );
 
+// if the current post has a featured image.
+if ( has_post_thumbnail( $args['post_id'] ) ) {
+
+	// set the image url to that of the featured image.
+	$args['image'] = get_the_post_thumbnail_url( $args['post_id'], 'full' );
+
+}
+
 // allow the args to be filtered.
 $args = apply_filters( 'ssi_wpjm_endpoint_generate_args', $args );
 
@@ -37,40 +45,52 @@ $template_location = apply_filters( 'ssi_wpjm_endpoint_generate_template_locatio
 ob_start();
 
 ?>
-<style>
-	:root{--hdsmi--text--color:white;--hdsmi--text--background-color:rgb(91, 212, 218);--hdsmi--background-color:rgb(23, 139, 145);--hdsmi--font-family:sans-serif;--hdsmi--font-size:6vw;--hdsmi--title--font-size:6vw;--hdsmi--title--font-weight:inherit;--hdsmi--title--text-transform:inherit;--hdsmi--location--font-size:4vw;--hdsmi--location--font-weight:inherit;--hdsmi--location--text-transform:inherit;--hdsmi--salary--font-size:3vw;--hdsmi--salary--font-weight:inherit;--hdsmi--salary--text-transform:inherit;--hdsmi--image--background-blend-mode:none;--hdsmi--logo--height:4vw;--hdsmi--logo--width:auto}body{margin:0;padding:0}.hdsmi-template{width:100vw;aspect-ratio:120/63;display:grid;place-items:center;background-color:var(--hdsmi--background-color)}.hdsmi-template__inner{position:relative;aspect-ratio:120/63;width:100%}.hdsmi-template__text{font-size:var(--hdsmi--font-size);font-family:var(--hdsmi--font-family);color:var(--hdsmi--text--color)}.hdsmi-template__title{color:var(--hdsmi--title--color,var(--hdsmi--text--color));font-size:var(--hdsmi--title--font-size, var(--hdsmi--font-size));font-weight:var(--hdsmi--title--font-weight);text-transform:var(--hdsmi--title--text-transform)}.hdsmi-template__location{color:var(--hdsmi--location--color,var(--hdsmi--text--color));font-size:var(--hdsmi--location--font-size, var(--hdsmi--font-size));font-weight:var(--hdsmi--location--font-weight);text-transform:var(--hdsmi--location--text-transform)}.hdsmi-template__salary{color:var(--hdsmi--salary--color,var(--hdsmi--text--color));font-size:var(--hdsmi--salary--font-size, var(--hdsmi--font-size));font-weight:var(--hdsmi--salary--font-weight);text-transform:var(--hdsmi--salary--text-transform)}.hdsmi-template__logo{height:var(--hdsmi--logo--height);width:var(--hdsmi--logo--width)}
-	.hdsmi-template{
-		--hdsmi--text--color: <?php echo esc_attr( $args['text_color'] ); ?>;
-		--hdsmi--text--background-color: <?php echo esc_attr( $args['bg_text_color'] ); ?>;
-		--hdsmi--background-color: <?php echo esc_attr( $args['bg_color'] ); ?>;
-		--hdsmi--title--font-size: <?php echo esc_attr( $args['title_size'] ); ?>vw;
-		--hdsmi--location--font-size: <?php echo esc_attr( $args['location_size'] ); ?>vw;
-		--hdsmi--salary--font-size: <?php echo esc_attr( $args['salary_size'] ); ?>vw;
-		--hdsmi--logo--height: <?php echo esc_attr( $args['logo_size'] ); ?>vw;
-		<?php if ( ! empty( $args['google_font_family'] ) ) { ?>--hdsmi--font-family: <?php echo $args['google_font_family']; ?>;<?php } ?>
-	}
-</style>
-<?php
+<!doctype html>
+<html class="no-js" lang="">
 
-// if we have a google font url.
-if ( ! empty( $args['google_font_family'] ) ) {
+	<head>
 
-	// output the link elements to load the font.
+		<link rel="stylesheet" href="<?php echo esc_url( SSI_WPJM_LOCATION_URL . '/assets/css/ssi-wpjm-generate.css' ); ?>" />
+		<style>
+			.hdsmi-template{
+				--hdsmi--text--color: <?php echo esc_attr( $args['text_color'] ); ?>;
+				--hdsmi--text--background-color: <?php echo esc_attr( $args['bg_text_color'] ); ?>;
+				--hdsmi--background-color: <?php echo esc_attr( $args['bg_color'] ); ?>;
+				--hdsmi--title--font-size: <?php echo esc_attr( $args['title_size'] ); ?>vw;
+				--hdsmi--location--font-size: <?php echo esc_attr( $args['location_size'] ); ?>vw;
+				--hdsmi--salary--font-size: <?php echo esc_attr( $args['salary_size'] ); ?>vw;
+				--hdsmi--logo--height: <?php echo esc_attr( $args['logo_size'] ); ?>vw;
+				<?php if ( ! empty( $args['google_font_family'] ) ) { ?>--hdsmi--font-family: <?php echo $args['google_font_family']; ?>;<?php } ?>
+			}
+		</style>
+		
+		<?php
+
+		// if we have a google font url.
+		if ( ! empty( $args['google_font_family'] ) ) {
+
+			// output the link elements to load the font.
+			?>
+
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="<?php echo esc_url( $args['google_font_url'] ); ?>" rel="stylesheet">
+
+			<?php
+
+		}
+
 	?>
 
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="<?php echo esc_url( $args['google_font_family'] ); ?>" rel="stylesheet">
+	</head>
 
-	<?php
-
-}
+<?php
 
 // if our template exists.
-if ( file_exists( $template_location . $args['template'] . '.php' ) ) {
+if ( file_exists( $template_location . $args['template'] . '.html' ) ) {
 
 	// load the template markup, passing our args.
-	load_template( $template_location . $args['template'] . '.php', true, $args );
+	load_template( $template_location . $args['template'] . '.html', true, $args );
 
 }
 
@@ -113,16 +133,26 @@ if ( $matches !== false ) {
 			// remove the tax: string
 			$match_key = str_replace( 'tax:', '', $match_value );
 
-			// get the value of this meta.
-			$match_value = wp_strip_all_tags(
-				get_the_term_list(
-					$args['post_id'],
-					$match_key,
-					'',
-					', ',
-					''
-				)
-			);
+			// if the taxonomy exists.
+			if ( taxonomy_exists( $match_key ) ) {
+				
+				// get the value of this meta.
+				$match_value = wp_strip_all_tags(
+					get_the_term_list(
+						$args['post_id'],
+						$match_key,
+						'',
+						', ',
+						''
+					)
+				);
+
+			} else {
+
+				// set empty match value.
+				$match_value = '';
+
+			}
 
 			// filter the match post value.
 			$match_value = apply_filters( 'ssi_wpjm_template_' . $match_key, $match_value, $match_key );
@@ -194,4 +224,4 @@ if ( $matches !== false ) {
 
 }
 
-echo $text;
+echo $text . '</body></html>';
