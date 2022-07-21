af4ea6754abbaf53915bb806d121791ae143725a
Mark Wilkinson <mark@highrise.digital>
2022-07-21 15:44:26 +0100

    Update version numbers and changelog

--- simple-social-images-wpjm.php
+++ simple-social-images-wpjm.php
@@ -3,7 +3,7 @@
  * Plugin Name: Simple Social Images for WP Job Manager
  * Plugin URI: https://simplesocialimages.com/wp-job-manager/
  * Description: Create automated, beautiful and branded images for jobs shared on social media channels. This plugin requires a license for <a href="https://simplesocialimages.com">Simple Social Images</a>.
- * Version: 1.0
+ * Version: 0.1
  * Author: Highrise Digital
  * Author URI: https://highrise.digital/
  * Text Domain: simple-social-images-wpjm
@@ -14,7 +14,7 @@
 // define variable for path to this plugin file.
 define( 'SSI_WPJM_LOCATION', dirname( __FILE__ ) );
 define( 'SSI_WPJM_LOCATION_URL', plugins_url( '', __FILE__ ) );
-define( 'SSI_WPJM_VERSION', '1.0' );
+define( 'SSI_WPJM_VERSION', '0.1' );
 
 /**
  * Function to run on plugins load.
